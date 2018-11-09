cb466f2a3c86db3d9b54613ea0bb6607d32e2ad3
Jeffrey <yyf921491129@163.com>
2018-11-09 17:45:45 +0800

    1

--- src/DotnetSpider.Sample/Program.cs
+++ src/DotnetSpider.Sample/Program.cs
@@ -1,8 +1,5 @@
-﻿using DotnetSpider.Downloader;
-using DotnetSpider.Sample.docs;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 
 namespace DotnetSpider.Sample
@@ -17,6 +14,8 @@ namespace DotnetSpider.Sample
 			ThreadPool.SetMinThreads(256, 256);
 #endif
 
+
+
 			//HttpClientDownloader downloader = new HttpClientDownloader();
 			//var response = downloader.Download(new Request("http://www.163.com")
 			//{
@@ -24,12 +23,33 @@ namespace DotnetSpider.Sample
 			//	Content = JsonConvert.SerializeObject(new { a = "bb" }),
 			//	ContentType = "application/json"
 			//});
-		
+
 			//AutoIncrementTargetRequestExtractorrSpider.Run();
 			//AfterDownloadCompleteHandlerSpider.Run();
+			TestSpider spider = new TestSpider();
+			spider.Run();
 		}
 
+		//// 合成
+		//public void Tts()
+		//{
+		//	// 设置APPID/AK/SK
+		//	var APP_ID = "你的 App ID";
+		//	var API_KEY = "你的 Api Key";
+		//	var SECRET_KEY = "你的 Secret Key";
 
+		//	var client = new Baidu.Aip.Speech.Tts(API_KEY, SECRET_KEY);
+		//	client.Timeout = 60000;
+		//	// 可选参数
+		//	var option = new Dictionary<string, object>() { };
+		//	var result = client.Synthesis("众里寻他千百度", option);
+
+		//	if (result.ErrorCode == 0)  // 或 result.Success
+		//	{
+		//		File.WriteAllBytes("合成的语音文件本地存储地址.mp3", result.Data);
+		//	}
+		//}
 
 	}
+
 }
--- src/DotnetSpider.Sample/TestSpider.cs
+++ src/DotnetSpider.Sample/TestSpider.cs
@@ -21,62 +21,51 @@ namespace DotnetSpider.Sample
 		protected override void OnInit(params string[] arguments)
 		{
 			Dictionary<string, object> headers = new Dictionary<string, object>();
-			headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-			headers.Add("Accept-Encoding", "gzip, deflate, br");
-			headers.Add("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8");
-			headers.Add("Cache-Control", "max-age=0");
-			headers.Add("Cookie", "_uab_collina=153110491333754395238005; _umdata=ED82BDCEC1AA6EB92406A172A4D19CAE3A36DDEC073F37020607F5D942FD7196A7E4978E7EC5EC42CD43AD3E795C914CA987280943182C85F5CF575E3A194823; htVD_2132_widthauto=-1; Hm_lvt_46d556462595ed05e05f009cdafff31a=1539768453,1540259209; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; htVD_2132_pc_size_c=0; htVD_2132_ulastactivity=1540262277%7C; htVD_2132_lastact=1540261763%09forum.php%09guide; Hm_lpvt_46d556462595ed05e05f009cdafff31a=1540262287");
+			headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");				
 			headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36");
-			AddHeaders("www.52pojie.cn", headers);
-			AddRequests("https://www.52pojie.cn/forum.php?mod=guide&view=hot&page=1");
+			headers.Add("Request Method", "POST");
+			AddHeaders("jzsc.mohurd.gov.cn", headers);
+			AddRequests("http://jzsc.mohurd.gov.cn/dataservice/query/comp/list?$pg=2");
 			EntityProcessor<BaiduSearchEntry> tempmodel = AddEntityType<BaiduSearchEntry>();
-			var pipeine = new XmlEntityPipeline();
-			pipeine.Title = "抓取吾爱破解最热文章";
-			pipeine.Description = "抓取吾爱破解最热文章";
-			pipeine.Link = "https://www.52pojie.cn/forum.php?mod=guide&view=hot&page=1";
+			var pipeine = new ExcelEntityPipeline();
+
 			AddPipeline(pipeine);
-			//AddEntityType<BaiduSearchEntry>().SetRequestExtractor(new AutoIncrementRequestExtractor("page=1"));
+			AddEntityType<BaiduSearchEntry>().SetRequestExtractor(new AutoIncrementRequestExtractor("$pg=2",1));
 		}
 
-		[Schema("baidu", "52POJIE")]
-		[Entity(Expression = ".//tbody ")]
+		[Schema("baidu", "mohurd")]
+		[Entity(Expression = ".//tr")]
 		public class BaiduSearchEntry : BaseEntity
 		{
 
-			//[Column]
-			//[Field(Expression = "Keyword", Type = SelectorType.Enviroment)]
-			//public string Keyword { get; set; }
-
 			[Column]
-			[Field(Expression = ".//a[@class='xst']")]
-			[ReplaceFormatter(NewValue = "", OldValue = "<em>")]
-			[ReplaceFormatter(NewValue = "", OldValue = "</em>")]
+			[Field(Expression = ".//td[@data-header='企业名称']/a")]
 			public string title { get; set; }
 
 			[Column]
-			[Field(Expression = ".//a[@class='xst']/@href")]
+			[Field(Expression = ".//td[@data-header='统一社会信用代码']")]
+			//[ReplaceFormatter(NewValue = "", OldValue = "<em>")]
+			//[ReplaceFormatter(NewValue = "", OldValue = "</em>")]
+			public string code { get; set; }
+
+			[Column]
+			[Field(Expression = ".//td[@data-header='企业名称']/a/@href")]
 			public string link { get; set; }
 
-			[Column]
-			[Field(Expression = ".//span[@class='xi1']")]
-			//[ReplaceFormatter(NewValue = "", OldValue = "人参与")]
-			public string guid { get; set; }
 
 
 			[Column]
-			[Field(Expression = ".//img[@alt='recommend']/@title")]
-			public string recommend { get; set; }
+			[Field(Expression = ".//td[@data-header='企业法定代表人']")]
+			public string people { get; set; }
+	
 
 			[Column]
-			[Field(Expression = ".//img[@alt='heatlevel']/@title")]
-			public string heatlevel { get; set; }
-			[Column]
-			[Field(Expression = ".//img[@alt='agree']/@title")]
-			public string agree { get; set; }
-
-			[Column]
-			[Field(Expression = ".//em/span")]
+			[Field(Expression = ".//td[@data-header='企业注册属地']")]
 			public string description { get; set; }
+
+			[Column]
+			[Field(Expression = ".//td[@data-header='序号']")]
+			public  int Id { get; set; }
 		}
 	}
 }
